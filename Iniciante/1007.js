1db7fb190c7a66423c99151c7bfc7e5222e69d58
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-05 12:30:51 -0300

    Create 1007.js

---
+++ Iniciante/1007.js
@@ -0,0 +1,8 @@
+let A = Number(lines.shift());
+let B = Number(lines.shift());
+let C = Number(lines.shift());
+let D = Number(lines.shift());
+
+let diferenca = (A*B-C*D);
+
+console.log("DIFERENCA = " + diferenca);
