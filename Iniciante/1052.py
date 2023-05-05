0ac7254ecfbfe51def0f4dedfd49dfc3e6c5a2fd
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-05 15:34:24 -0300

    Create 1052.py

---
+++ Iniciante/1052.py
@@ -0,0 +1,26 @@
+valor = int(input())
+
+if valor == 1:
+    print("January")
+elif valor == 2:
+    print("February")
+elif valor == 3:
+    print("March")
+elif valor == 4:
+    print("April")
+elif valor == 5:
+    print("May")
+elif valor == 6:
+    print("June")
+elif valor == 7:
+    print("July")
+elif valor == 8:
+    print("August")
+elif valor == 9:
+    print("September")
+elif valor == 10:
+    print("October")
+elif valor == 11:
+    print("November")
+elif valor == 12:
+    print("December")
