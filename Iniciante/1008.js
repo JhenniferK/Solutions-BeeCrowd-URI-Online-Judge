bc497fdbe7598bd4d444641d75bb93c56dcd5f43
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-05 12:33:39 -0300

    Create 1008.js

---
+++ Iniciante/1008.js
@@ -0,0 +1,8 @@
+let n = Number(lines.shift());
+let ht = Number(lines.shift());
+let v = Number(lines.shift());
+
+let salario = ht*v;
+
+console.log("NUMBER = " + n);
+console.log("SALARY = U$ " + salario.toFixed(2));
