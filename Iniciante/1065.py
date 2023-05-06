333768c3d95cfec88d73c0ce53abb59e9c1f65ba
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-06 17:18:26 -0300

    Update 1065.py

--- Iniciante/1065.py
+++ Iniciante/1065.py
@@ -1,8 +1,8 @@
-contPos = 0
+contPares = 0
 
 for i in range(5):
   valor = int(input())
   if valor%2 == 0:
-    contPos += 1
+    contPares += 1
     
-print("{} valores pares".format(contPos))
+print("{} valores pares".format(contPares))
