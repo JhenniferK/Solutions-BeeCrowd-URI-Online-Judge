b80910fe5eb3df46af51044b6afcf58648a04661
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-08 17:11:40 -0300

    Create 1142.js

---
+++ Iniciante/1142.js
@@ -0,0 +1,11 @@
+let lim = lines.shift();
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+
+for (let i = 0; i < lim; i++) {
+  console.log(n1 + ' ' + n2 + ' ' + n3 + " PUM");
+  n1 += 4;
+  n2 += 4;
+  n3 += 4;
+}
