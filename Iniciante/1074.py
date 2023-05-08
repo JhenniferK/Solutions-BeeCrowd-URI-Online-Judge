87c0b29110eb5dd2610ad5dc979f6527fc2c1b55
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-07 21:06:37 -0300

    Create 1074.py

---
+++ Iniciante/1074.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    num = int(input())
+    if num > 0 and num%2 == 0:
+        print("EVEN POSITIVE")
+    elif num > 0 and num%2 != 0:
+        print("ODD POSITIVE")
+    elif num == 0:
+        print("NULL")
+    elif num < 0 and num%2 == 0:
+        print("EVEN NEGATIVE")
+    elif num < 0 and num%2 != 0:
+        print("ODD NEGATIVE")
