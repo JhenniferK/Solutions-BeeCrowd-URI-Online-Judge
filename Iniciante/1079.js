9201db69c621f08fa02e09a47292a9d3ce15dfea
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-07 21:11:48 -0300

    Create 1079.js

---
+++ Iniciante/1079.js
@@ -0,0 +1,14 @@
+let l1 = Number(lines.shift());
+let valores = new Array(3);
+
+for (let i = 0; i < l1; i++) {
+  valores = lines.shift().split(' ').map(Number);
+  let soma = 0, pesos = [2, 3, 5];
+
+  for (let j = 0; j < valores.length; j++) {
+    soma += valores[j]*pesos[j];
+  }
+
+  let media = soma/10;
+  console.log(media.toFixed(1));
+}
