a7ed72dc5d6fe64d52bbad9b409ae003b4580122
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-05-16 11:48:08 -0300

    Update 2344.js

--- Iniciante/2344.js
+++ Iniciante/2344.js
@@ -1,23 +1,13 @@
-let conceito = new Array(101);
+let nota = Number(lines.shift())
 
-conceito[0] = "E";
-
-for (let i = 1; i <= 35; i++) {
-  conceito[i] = "D";
+if (nota == 0) {
+    console.log("E");
+} else if (nota >= 1 && nota <= 35) {
+    console.log("D");
+} else if (nota > 35 && nota <= 60) {
+    console.log("C");
+} else if (nota > 60 && nota <= 85) {
+    console.log("B");
+} else {
+    console.log("A");
 }
-
-for (let i = 36; i <= 60; i++) {
-  conceito[i] = "C";
-}
-
-for (let i = 61; i <= 85; i++) {
-  conceito[i] = "B";
-}
-
-for (let i = 86; i <= 100; i++) {
-  conceito[i] = "A";
-}
-
-let nota = Number(lines.shift());
-
-console.log(conceito[nota]);
