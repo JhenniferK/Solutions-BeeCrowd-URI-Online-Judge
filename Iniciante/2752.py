4ccf5ec6a81330a1d76c0592d976610b8015b8f8
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-07-27 14:29:36 -0300

    Create 2752.py

---
+++ Iniciante/2752.py
@@ -0,0 +1,10 @@
+str = "AMO FAZER EXERCICIO NO URI";
+
+print("<{}>".format(str));
+print("<{:>30}>".format(str));
+print("<{:.20}>".format(str));
+print("<{:<20}>".format(str));
+print("<{:<30}>".format(str));
+print("<{:.30}>".format(str));
+print("<{:>30.20}>".format(str));
+print("<{:<30.20}>".format(str));
