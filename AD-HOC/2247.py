881f7143414673ec76e2bbbda521f69757d71db0
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-08-01 15:17:52 -0300

    Create 2247.py

---
+++ AD-HOC/2247.py
@@ -0,0 +1,20 @@
+teste = 1;
+
+while True:
+    n = int(input());
+    
+    if n == 0:
+        break;
+        
+    valor = 0;
+    
+    print("Teste {}".format(teste));
+    teste += 1;
+    
+    while n:
+        n -= 1;
+        j, z = [int(x) for x in input().split()];
+        valor += (j - z);
+        print(valor);
+
+    print();
