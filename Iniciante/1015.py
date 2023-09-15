3b351f74e1810b22f560762e11b1c362efc03310
JhenniferKelly <132682121+JhenniferK@users.noreply.github.com>
2023-09-15 14:59:52 -0300

    Update 1015.py

--- Iniciante/1015.py
+++ Iniciante/1015.py
@@ -1,13 +1,8 @@
 import math
 
-linha1 = input().split()
-linha2 = input().split()
+x1, y1 = list(map(float, input().split()));
+x2, y2 = list(map(float, input().split()));
 
-x1 = float(linha1[0])
-y1 = float(linha1[1])
-x2 = float(linha2[0])
-y2 = float(linha2[1])
+distancia = math.sqrt((x2-x1)**2 + (y2-y1)**2);
 
-distancia = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-print("{:.4f}".format(distancia))
+print("{:.4f}".format(distancia));
